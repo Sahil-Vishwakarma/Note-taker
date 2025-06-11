6daea3fab7fb99b2ac7a81206b8adab42fe22978
sahil <sahil0000002@gmail.com>
2025-06-11 17:38:20 +0530

    feat: add edit functionality for file management with form handling

--- index.js
+++ index.js
@@ -44,4 +44,43 @@ app.get('/file/:filename', (req, res) => {
         res.render('info', { content: data, filename: req.params.filename });
     })  
 })
+
+app.get('/edit/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading file');
+        }
+        const fileName = req.params.filename.split('.')[0];
+        res.render('edit', { content: data, filename: fileName });
+    })  
+})
+
+app.post('/edit/done/:filename', (req, res) => {
+    if( !req.body.title || !req.body.details) {
+        return res.status(400).send('Title and details are required');
+    }
+
+    const existingFileName = path.join('./files', req.params.filename + '.txt');
+    const newFileName = path.join('./files', req.body.title.trim()
+                        .split(' ')
+                        .map((word) => word[0].toUpperCase() + word.slice(1))
+                        .join('') + '.txt');
+
+    fs.rename(existingFileName, newFileName, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error renaming file');
+        }
+
+        fs.writeFile(newFileName, req.body.details.trim(), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error writing file');
+            }
+            res.redirect('/');
+        });
+    });
+});
+
 app.listen(3000);
