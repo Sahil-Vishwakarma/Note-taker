4fb96368fb8561bacd278c7e8bf226a7fb4c7d2a
sahil <sahil0000002@gmail.com>
2025-06-11 16:49:29 +0530

    feat: implement file management with create and view functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
@@ -8,7 +9,35 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine' , 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readdir('./files', (err, files) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading files');
+        }
+       res.render('index', { files: files });
+    });
 })
 
+app.post('/create', (req, res) => {
+    const fileName = req.body.title.split(' ')
+                        .map((word) => word[0].toUpperCase() + word.slice(1))
+                        .join('');
+    fs.writeFile(`./files/${fileName}.txt`, req.body.details, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error creating file');
+        }
+        res.redirect('/');
+    })
+})
+
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading file');
+        }
+        res.render('info', { content: data, filename: req.params.filename });
+    })  
+})
 app.listen(3000);
